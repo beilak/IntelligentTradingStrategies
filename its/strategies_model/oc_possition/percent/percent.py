212d4eca0ef526f78d055f23c687772dba6cd9a3
Beilak <beylak@yandex.ru>
2024-03-24 10:15:17 +0300

    add close position of precent strat

--- its/strategies_model/oc_possition/percent/percent.py
+++ its/strategies_model/oc_possition/percent/percent.py
@@ -1,16 +1,16 @@
 from decimal import Decimal
 from typing import TypedDict
-from stsp import OpenPositionProtocol
+from stsp import ExitPointResponse, OpenPositionProtocol, ClosePositionProtocol
 from stsp.protocols.position.models import EntryPointResponse
 from stsp.protocols.predict.models import PredictResponse
 
 
-class OpenPosstionDto(TypedDict):
+class PosstionDto(TypedDict):
     percent: int
 
 
 class OpenPossitionByPredictPrec(OpenPositionProtocol):
-    def __init__(self, dto: OpenPosstionDto) -> None:
+    def __init__(self, dto: PosstionDto) -> None:
         self._percent = dto["percent"]
 
     async def definition_entry_point(
@@ -25,5 +25,25 @@ class OpenPossitionByPredictPrec(OpenPositionProtocol):
             price=Decimal(
                 prediction["next_price"]
                 - (prediction["next_price"] * self._percent / 100)
+            )
+        )
+
+
+class ClosePossitionByPredictPrec(ClosePositionProtocol):
+    def __init__(self, dto: PosstionDto) -> None:
+        self._percent = dto["percent"]
+
+    async def definition_exit_point(
+        self,
+        *,
+        ticker: str,
+        prediction: PredictResponse,
+    ) -> EntryPointResponse:
+        """Provide position entry point"""
+
+        return ExitPointResponse(
+            price=Decimal(
+                prediction["next_price"]
+                + (prediction["next_price"] * self._percent / 100)
             )
         )
--- tests/strategies_model/oc_possition/percent/test_percent.py
+++ tests/strategies_model/oc_possition/percent/test_percent.py
@@ -1,14 +1,15 @@
 from decimal import Decimal
-from stsp import EntryPointResponse, PredictResponse
+from stsp import EntryPointResponse, ExitPointResponse, PredictResponse
 from stsp.protocols.predict.models import datetime
 from its.strategies_model.oc_possition.percent.percent import (
     OpenPossitionByPredictPrec,
-    OpenPosstionDto,
+    ClosePossitionByPredictPrec,
+    PosstionDto,
 )
 
 
 async def test_percent_definition_entry_point() -> None:
-    model = OpenPossitionByPredictPrec(dto=OpenPosstionDto(percent=10))
+    model = OpenPossitionByPredictPrec(dto=PosstionDto(percent=10))
 
     resp: EntryPointResponse = await model.definition_entry_point(
         ticker="TEST",
@@ -18,3 +19,16 @@ async def test_percent_definition_entry_point() -> None:
         ),
     )
     assert resp["price"] == 900
+
+
+async def test_percent_definition_exit_point() -> None:
+    model = ClosePossitionByPredictPrec(dto=PosstionDto(percent=10))
+
+    resp: ExitPointResponse = await model.definition_exit_point(
+        ticker="TEST",
+        prediction=PredictResponse(
+            for_date=datetime.now(),
+            next_price=Decimal(1000),
+        ),
+    )
+    assert resp["price"] == 1100
